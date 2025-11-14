ad6e011a884c2eb4fad07e61b8273ef515c85650
Tomke Folger <tomkefolger@MacBook-Air-von-Tomke.local>
2025-11-14 21:52:23 +0100

    add db options allowed

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=cors_origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
